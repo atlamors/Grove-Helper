0f34716593abcdc5482d3184cf2e59ecbbde2858
Andrew Nelson <andrew@anothercreative.ca>
2021-09-13 00:38:42 +0000

    Depositing gold and updating gold working

--- functions/deposit.js
+++ functions/deposit.js
@@ -11,13 +11,15 @@ class Deposit {
 	 * @param {*} user 
 	 * @param {*} guild 
 	 */
-	async insertMember(user, guild) {
+	async insertMember(user, guild, deposit) {
 		const crud = new CRUD()
 		
 		guild = guild.id
 		user = {
 			"_id" : user.id,
 			"name" : user.tag,
+			"deposit" : deposit,
+			"total" : deposit,
 		}
 
 		crud.create(user, guild) 
@@ -75,6 +77,36 @@ class Deposit {
 
 	/**
 	 * 
+	 * @param {*} user 
+	 * @param {*} guild 
+	 * @returns 
+	 */
+	async confirmMember(user, guild) {
+		const crud = new CRUD()
+
+		guild = guild.id
+		user = {
+			"_id" : user.id,
+		}
+
+		return await ( crud.check(user, guild) ) ? true : false
+	}
+
+	/**
+	 * 
+	 * @param {*} user 
+	 * @param {*} guild 
+	 * @returns 
+	 */
+	async checkPermissions(user, guild) {
+		return await guild.members.fetch(user.id).then( member => {
+			return ( member.roles.cache.has('883400308447916053') ) ? true : false
+		})
+	}
+
+
+	/**
+	 * 
 	 * @param {*} message 
 	 * @param {*} reactions 
 	 */
@@ -82,23 +114,6 @@ class Deposit {
 		reactions.forEach( e => { 
 			message.react(e)
 		});
-	}
-
-	/**
-	 * 
-	 * @param {*} user 
-	 * @param {*} guild 
-	 */
-	async checkPermissions(user, guild) {
-		const perms = await guild.members.fetch(user.id).then( member => {
-			const roles = member.roles.cache.has('883400308447916053')
-			if ( roles == true ) {
-				return true
-			} else {
-				return false
-			}
-		})
-		return perms
 	}
 
 	/**
@@ -123,35 +138,30 @@ class Deposit {
 	 * @param {*} guild 
 	 * @param {*} data 
 	 */
-	async updateMemberTotals(user, guild, deposit$) {
-		const member = await this.readMember(user, guild)
+	async updateMemberTotals(user, guild, deposit) {
+		const data = await this.readMember(user, guild)
 
-		if (!member) this.insertMember(user, guild)
+		data["deposit"] = deposit
+		data["total"] = data.total + deposit
 
-		console.log(member)		
-
-		//this.updateMember(user, guild, data)
-		
-		// guild = guild.id
-		// user = {
-		// 	"_id" : user.id,
-		// }
-
+		this.updateMember(user, guild, data)
 	}
 
 	async collector(collector, interaction, _user, reaction, embedMessage, reactions) {
 		const guild 	= interaction.guild 		//Guild command was issued in
 		const user		= interaction.member.user 	//User who initially issued commands
-		const deposit$ 	= interaction.options.getInteger('deposit')
-		const perms 	= await this.checkPermissions(_user, guild);
-		if ( perms == true ) {
+		const deposit 	= interaction.options.getInteger('deposit')
+
+		if ( await this.checkPermissions(_user, guild) ) {
 			console.log(`Approved! ${_user.tag} has permissions.`)
 			collector.stop()
 			this.updateConfirmationReaction(_user, reaction, embedMessage, reactions)
-			this.updateMemberTotals(user, guild, deposit$)
+			this.confirmMember(user, guild).then( r => {
+				(r) ? this.updateMemberTotals(user, guild, deposit) : this.insertMember(user, guild, deposit)
+			})	
 		} else {
 			console.log(`${_user.tag} does not have permissions.`)
-			reaction.users.remove(_user.id)
+				reaction.users.remove(_user.id)
 		}
 	}
 }
